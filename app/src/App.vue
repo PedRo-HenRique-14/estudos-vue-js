a668c192a18317c9622af24944460339e12a0326
Dodas_ <ph005009004@gmail.com>
2024-08-19 14:34:45 -0300

    add a simple header

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,15 +1,18 @@
 <template>
+  <HeaderNav></HeaderNav>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import HeaderNav from './components/HeaderNav.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    HeaderNav,
   }
 }
 </script>
