286c9c5a8b8da598729d51430481bc0c15739506
Dodas_ <ph005009004@gmail.com>
2024-08-20 15:03:55 -0300

    test v-for

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,36 +1,52 @@
 <template>
-  <HeaderNav v-if="showHeader" />
-  
-  <div v-show="showName">
-    Nome: {{ firstName }} <br>
-    Sobrenome: {{ lastName }}
+  <div>
+    <div 
+      v-for="(obj, index) in todos"
+      v-bind:key="obj.id"
+      class="todo-item"
+    >
+      {{ index + 1 }} - {{ obj.title }}
+    </div>
   </div>
-
-  <div v-if="accessLevel == 'admin'">Admin User</div>
-  <div v-else-if="accessLevel == 'marketing'">Marketing User</div>
-  <div v-else>Default User</div>
-
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import HeaderNav from './components/HeaderNav.vue';
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld,
-    HeaderNav,
-  },
   data() {
     return {
-      showHeader: true,
-      firstName: 'Pedro Henrique',
-      lastName: 'Garcia Schuch',
-      showName: false,
-      accessLevel: 'admin',
+      todos: [
+  {
+    "userId": 1,
+    "id": 1,
+    "title": "delectus aut autem",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 2,
+    "title": "quis ut nam facilis et officia qui",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 3,
+    "title": "fugiat veniam minus",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 4,
+    "title": "et porro tempora",
+    "completed": true
+  },
+  {
+    "userId": 1,
+    "id": 5,
+    "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
+    "completed": false
+  }
+]
     }
   }
 }
@@ -38,11 +54,18 @@ export default {
 
 <style>
 #app {
+  display: flex;
+  justify-content: center;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.todo-item{
+  background-color: black;
+  color: white;
+  margin: 10px;
+  padding: 15px;
 }
 </style>
