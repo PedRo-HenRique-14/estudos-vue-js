4604d45740937cfdb80a796990e7af91d904bd3b
Dodas_ <ph005009004@gmail.com>
2024-08-21 23:12:57 -0300

    use events and events modifiers

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,45 +1,18 @@
 <template>
   <div>
-    <div>
-      Two-Way data binding <br>
-      v-model -> formulários <br><br>
-      <div>
-        <label for="">Nome:</label>
-        <input type="text" v-model="name"> <br>
-        {{ name }}
-      </div> <br><br>
-      <div>
-        <label for="">Esportes:</label>
-        <select v-model="sports">
-          <option value="">Selecione</option>
-          <option value="Futebol">Futebol</option>
-          <option value="Vôlei">Vôlei</option>
-          <option value="Ping-Pong">Ping-Pong</option>
-        </select><br>
-        {{ sports }}
-      </div> <br>
-      <div>
-        <label for="">Newsletter</label><br>
-        <input v-model="newsletter" type="radio" value="Sim">Sim <br>
-        <input v-model="newsletter" type="radio" value="Não">Não <br>
-        {{ newsletter }}
-      </div>
-      <br><br>
-      <div>
-        <label for="">Termos de serviço</label><br>
-        <input v-model="terms" type="checkbox">Concordo com tudo :D <br>
-        {{ terms }}
-      </div>
-      <br><br>
-      <div>
-        <label for="">Cores que você mais gosta</label><br>
-        <input v-model="colors" type="checkbox" value="Roxo">Roxo <br>
-        <input v-model="colors" type="checkbox" value="Rosa">Rosa <br>
-        <input v-model="colors" type="checkbox" value="Azul">Azul <br>
-        <input v-model="colors" type="checkbox" value="Amarelo">Amarelo <br>
-        {{ colors }}
-      </div>
+    <button @click="onClick">Enviar</button>
+    <br>
+    <div @mouseover="mouseOver" @mouseout="mouseOut">
+      Mouse over
     </div>
+    <br><br>
+    <form 
+      action="https://google.com"
+      @submit.prevent="submitForm"
+    >
+      <input type="text" @keyup="onKeyUp">
+      <button type="submit">Enviar</button>
+    </form>
   </div>
 </template>
 
@@ -48,13 +21,26 @@ export default {
   name: 'App',
   data() {
     return {
-      name:'Pedro Henrique',
-      sports: '',
-      newsletter: 'Sim',
-      terms: true,
-      colors: [],
+      
     }
-  }
+  },
+  methods: {
+    onClick(evt) {
+      console.log('Olha o click!', evt)
+    },
+    mouseOver(evt) {
+      console.log('Mouse over!!!', evt)
+    },
+    mouseOut(evt) {
+      console.log('Mouse out!!!', evt)
+    },
+    submitForm(evt) {
+      console.log('Form Enviado!!!', evt)
+    },
+    onKeyUp(evt) {
+      console.log( `${evt.key} key up!!!`, evt)
+    },
+  },
 }
 </script>
 
