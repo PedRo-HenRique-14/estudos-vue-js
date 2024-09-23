e2dc5ededcf7d6a43cf8e1be29625303890f2435
Dodas_ <ph005009004@gmail.com>
2024-09-23 16:48:05 -0300

    use component slot

--- app/src/components/HeaderNav.vue
+++ app/src/components/HeaderNav.vue
@@ -1,6 +1,13 @@
 <template>
     <header class="header">
-        Header
+        <!-- A tag slot possibilita inserir dados através da tag <HeaderNav></HeaderNav> no componente App -->
+        <h1 v-if="$slots.title" class="title"> <!-- <h1> só será criado se v-if for verdadeiro -->
+            <slot name="title"/>
+        </h1>
+        <div class="description">
+            <slot name="description"/>
+        </div>
+        <div class="content"><slot/></div>
     </header>
 </template>
 <script>
@@ -9,8 +16,15 @@
     }
 </script>
 <style>
+    * {
+        margin: 0px;
+        padding: 0px;
+    }
     .header {
         background-color: #000;
         color: #fff;
+        margin: 0px;
+        padding: 10px 20px;
+        width: calc(100vw - 40px);
     }
 </style>
